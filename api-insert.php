ba9a3593dc3a977d454f59a1a28cd4d34b5db28b
Mahabubur Rahman <mahabubmon@gmail.com>
2023-12-13 19:47:15 +0600

    data_insert_by_api

--- api-insert.php
+++ api-insert.php
@@ -1,4 +1,28 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers:Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+
+$uname = $data['uname'];
+$uusername = $data['uusername'];
+$uemail = $data['uemail'];
+$uaddress = $data['uaddress'];
+
+include 'db.php';
+
+$sql = "INSERT INTO user_tbl(name, username, email, address) VALUES ('{$uname}','{$uusername}','{$uemail}','{$uaddress}')";
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array('message' => 'User Record Inserted.', 'status' => true));
+
+} else {
+    echo json_encode(array('message' => 'User Record Not Inserted.', 'status' => false));
+}
 
 
 ?>
